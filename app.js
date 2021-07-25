f509f5d1c12016c15e49b7098b42c9e3f116164a
Megan Fuhr <MeganFuhr@gmail.com>
2021-07-25 09:03:44 -0400

    :poop: testing
    testing

--- app.js
+++ app.js
@@ -274,7 +274,7 @@ function startSleepInterval() {
 function checkIfSleepy(){
 	var t = new Date()
 	var currentTime = t.getUTCHours()
-	if(currentTime > 0 && currentTime < 12){
+	if(currentTime > 0 && currentTime < 14){
 		console.log(`On the server: J is tired.  Please turn off the lights.`)
 		jIsSleepy = true
 
